24fff7faa68d1202c09a3f6a794684dafb11389e
True Merrill <truemerrill@gmail.com>
2022-08-27 08:35:36 -0400

    Added lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -61,6 +61,9 @@ return packer.startup(function (use)
   use {"kyazdani42/nvim-tree.lua",
     requires = "kyazdani42/nvim-web-devicons"
   }
+  use {"nvim-lualine/lualine.nvim",
+    requires = "kyazdani42/nvim-web-devicons"
+  }
 
   -- Completion
   use "hrsh7th/nvim-cmp"                -- Completion engine
